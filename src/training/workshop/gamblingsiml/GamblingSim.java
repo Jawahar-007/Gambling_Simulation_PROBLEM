0bc8e7bb1fdaf0e6741719f492b87349cc2c82c1
Jawahar-007 <jawaharbhaskaran@gmail.com>
2024-01-25 15:21:25 +0530

    Changes done

--- src/training/workshop/gamblingsiml/GamblingSim.java
+++ src/training/workshop/gamblingsiml/GamblingSim.java
@@ -2,13 +2,13 @@ package training.workshop.gamblingsiml;
 
 import java.sql.SQLOutput;
 import java.util.Random;
-class stake {
+class GamblingSim {
     public int day_stake;
     public int game_stake;
     public int numOfDays;
     public int stake_amount;
 
-    public stake() {
+    public GamblingSim() {
         day_stake = 100;
         game_stake = 1;
     }
@@ -17,33 +17,36 @@ class stake {
     public String toString() {
         return "stake{" + "Day_Stake =" +"Game_Stake = " + game_stake + '}';
     }
-}
-
-    public class GamblingSim {
-
 
     public void betprobability() {
         int stake_amount = 0;
         int i;
         int daytotalwin = 0;
         int daytotalloose = 0;
+        int betcheck;
         Random random = new Random();
-        daytotalloose = random.nextInt(2);
-            for (i = 1; i <= 100; i++) {
-                if (daytotalwin == 1) {
-                    stake_amount += 1;
-                    System.out.print("Stake Win: " + daytotalwin);
-                } else {
-                    stake_amount += 1;
-                    System.out.println(" Total Stake Loss:" + daytotalloose);
+
+        for (i = 1; i <= 100; i++) {
+            betcheck = random.nextInt(2);
+            if (betcheck == 1) {
+                stake_amount += 1;
+                daytotalwin ++;
+                System.out.print("Stake Win: "+stake_amount+"DAY WIN " + daytotalwin);
+                if (daytotalwin == 50) {
+                    System.out.println("Resigning with win");
+                    break;
+                }
+            } else {
+                stake_amount += 1;
+                daytotalloose++;
+                System.out.println(" Total Stake Loss:" + daytotalloose);
+                if (daytotalloose == 50) {
+                    System.out.println("Lost 50%!!Dissatisfied!Leaving");
+                    break;
                 }
             }
-         if(stake_amount >= 50)
-            System.out.println("Won 50% Stake!!Satisfied ! Leave");
-         else
-            System.out.println("Lost 50%!!Dissatisfied!Leaving");
         }
-
+    }
     public static void main(String[] args){
         GamblingSim ob = new GamblingSim();
         ob.betprobability();
