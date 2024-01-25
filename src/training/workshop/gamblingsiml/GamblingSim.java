630fab524ef1e4b48d4208919014a7ebf80c76d5
Jawahar-007 <jawaharbhaskaran@gmail.com>
2024-01-25 14:34:53 +0530

    50% done for regignation

--- src/training/workshop/gamblingsiml/GamblingSim.java
+++ src/training/workshop/gamblingsiml/GamblingSim.java
@@ -1,9 +1,12 @@
 package training.workshop.gamblingsiml;
 
+import java.sql.SQLOutput;
 import java.util.Random;
 class stake {
     public int day_stake;
     public int game_stake;
+    public int numOfDays;
+    public int stake_amount;
 
     public stake() {
         day_stake = 100;
@@ -17,7 +20,7 @@ class stake {
 }
 
     public class GamblingSim {
-    public int stake_amount;
+
 
     public void betprobability() {
         int stake_amount = 0;
@@ -25,19 +28,24 @@ class stake {
         int daytotalwin = 0;
         int daytotalloose = 0;
         Random random = new Random();
-            daytotalloose = random.nextInt(2);
-            if (daytotalloose == 1) {
-                stake_amount -= 1;
-                System.out.print("Stake Loose: "+ daytotalloose);
-            } else {
-                stake_amount += 1;
-                System.out.println( " Total Win Stake :" + daytotalwin);
+        daytotalloose = random.nextInt(2);
+            for (i = 1; i <= 100; i++) {
+                if (daytotalwin == 1) {
+                    stake_amount += 1;
+                    System.out.print("Stake Win: " + daytotalwin);
+                } else {
+                    stake_amount += 1;
+                    System.out.println(" Total Stake Loss:" + daytotalloose);
+                }
             }
+         if(stake_amount >= 50)
+            System.out.println("Won 50% Stake!!Satisfied ! Leave");
+         else
+            System.out.println("Lost 50%!!Dissatisfied!Leaving");
         }
 
     public static void main(String[] args){
         GamblingSim ob = new GamblingSim();
         ob.betprobability();
-
     }
 }
